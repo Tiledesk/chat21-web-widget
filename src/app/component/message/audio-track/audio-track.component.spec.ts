de8d54671270a7abb6a79f7916320c4b5d922943
Gabriele Panico <gabriele.panico@gmail.com>
2025-05-16 17:25:41 +0200

    first commit

---
+++ src/app/component/message/audio-track/audio-track.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AudioTrackComponent } from './audio-track.component';
+
+describe('AudioTrackComponent', () => {
+  let component: AudioTrackComponent;
+  let fixture: ComponentFixture<AudioTrackComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AudioTrackComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AudioTrackComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
