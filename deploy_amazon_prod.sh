f572c8f2f2f343ff582ab52e0810bd4b5ceebf23
Dario De Pascalis <dariodepa75@gmail.com>
2025-10-06 16:03:44 +0200

    version added: ### 5.1.4

--- deploy_amazon_prod.sh
+++ deploy_amazon_prod.sh
@@ -2,8 +2,18 @@
 version=`node -e 'console.log(require("./package.json").version)'`
 echo "version $version"
 
+npm i
+
+cp src/environments/real_data/environment.prod.ts src/environments/environment.prod.ts
+
 # --build-optimizer=false if localstorage is disabled (webview) appears https://github.com/firebase/angularfire/issues/970
 ng build --configuration="prod" --aot=true
+##--base-href='./v5/' --output-hashing none
+
+### SET HASHING : START ###
+cp ./src/launch_template.js ./dist/browser/launch.js
+node ./src/build_launch.js
+### SET HASHING : END ###
 
 #### FIREBASE #####
 # cd dist
@@ -13,14 +23,18 @@ ng build --configuration="prod" --aot=true
 # cd ..
 
 # #### MQTT #####
-cd dist
+cd dist/browser
 # aws s3 sync . s3://tiledesk-widget/v5/latest/
-aws s3 sync . s3://tiledesk-widget/v6/$version/ --cache-control max-age=300
-#aws s3 sync . s3://tiledesk-widget/v6/ --cache-control max-age=300
-cd ..
-
+aws s3 sync . s3://tiledesk-widget/v6/$version/ --cache-control max-age=86400 --exclude='launch.js' #8days
+aws s3 sync . s3://tiledesk-widget/v6/$version/ --cache-control "no-store,no-cache,private" --exclude='*' --include='launch.js'
+aws s3 sync . s3://tiledesk-widget/v6/ --cache-control max-age=86400 --exclude='launch.js' #8days
+aws s3 sync . s3://tiledesk-widget/v6/ --cache-control "no-store,no-cache,private" --exclude='*' --include='launch.js'
+cd ../..
 
 aws  cloudfront create-invalidation --distribution-id E3EJDWEHY08CZZ --paths "/*"
+
+git restore src/environments/environment.pre.ts
+
 echo new version deployed $version on s3://tiledesk-widget/v5
 echo available on https://s3.eu-west-1.amazonaws.com/tiledesk-widget/v5/index.html
 echo https://widget.tiledesk.com/v5/index.html
